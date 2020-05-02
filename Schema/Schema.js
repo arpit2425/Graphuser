2b0ef7f8888d62a30f6796084718ee067cf32446
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-02 20:11:51 +0530

    hooking company to root query

--- Schema/Schema.js
+++ Schema/Schema.js
@@ -38,6 +38,15 @@ const RootQuery = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 module.exports = new GraphQLSchema({
